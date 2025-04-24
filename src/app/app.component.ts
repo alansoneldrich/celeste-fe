b662be04de60ca88992d6f31ede16694de2b2c99
alansoneldrich <alansoneldrich@gmail.com>
2025-04-24 16:45:08 +0800

    Daisy import

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ApiService } from './services/api.service';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'celeste-fe';
+  memory: { sender: string, text: string, timestamp?: string }[] = [];
+
+  constructor(private apiService: ApiService) {}
+
+  ngOnInit(): void {
+    this.apiService.getMemory().subscribe({
+      next: (res) => {
+        this.memory = res.messages || [];
+      },
+      error: () => {
+        this.memory = [];
+      }
+    });
+  }
 }
